755cec324abc6a376b0a6c7690e50e0221d5befe
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-23 16:52:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,28 @@
 # Make your shoe class here!
 class Shoe
 
-def initialize(brand)
-  @brand = brand
-end
+  def initialize(brand)
+    @brand = brand
+  end
 
-def brand
-  @brand
-end
+  def brand
+    @brand
+  end
 
-def color=(color)
-  @color = color
-end
+  def color=(color)
+    @color = color
+  end
 
-def color
-  @color
-end
+  def color
+    @color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
 
 end
